cbebca2b888397a7a42618dbc1d52fa4edea06a8
Alvaro Neira <alvaroneirareyes@gmail.com>
2015-10-03 20:04:44 -0300

    Fixed first saccade

--- matlab/fullProcess.m
+++ matlab/fullProcess.m
@@ -101,17 +101,18 @@ end
 pupil_area_fixed=pupil_area;
 for i=1:length(timestamp)
     s=char(saccade(i));
-        if length(s) > 0 && strcmp(s(1:4),'DIR_')
-        delta=pupil_area(i+1)-pupil_area(i); 
-        pupil_area_fixed(i)=pupil_area(i-2);
-        pupil_area_fixed(i-1)=pupil_area(i-2);
-        for j=i+1:length(timestamp)-1
-            pupil_area_fixed(j)=pupil_area_fixed(j-1)+delta;
-                delta=pupil_area(j+1)-pupil_area(j);
+    if length(s) > 0 && strcmp(s(1:4),'DIR_')
+        for k=i:length(timestamp)-1
+            delta(k)=pupil_area_fixed(k+1)-pupil_area_fixed(k);
+        end    
+        pupil_area_fixed(i)=pupil_area_fixed(i-2);
+        pupil_area_fixed(i-1)=pupil_area_fixed(i-2);
+        for j=i:length(timestamp)-1
+            pupil_area_fixed(j+1)=pupil_area_fixed(j)+delta(j);
         end
         break;
-    end
-    end
+     end
+end
 
  plot(1:length(original_pupil_area),original_pupil_area,'r',1:length(pupil_area),pupil_area_fixed,'b')
 
