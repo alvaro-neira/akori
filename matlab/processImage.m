2a92b6f5c12f50eac6e80bef74960dd152e970dd
Alvaro Neira <alvaroneirareyes@gmail.com>
2015-10-14 19:23:08 -0300

    Detecting rectangles 1 line vertical

--- matlab/processImage.m
+++ matlab/processImage.m
@@ -1,18 +1,20 @@
 B1=imread('/Users/aneira/lalo/data/pngs/science_mag_mask.bmp');
 B=transpose(B1(:,:,1));%permute(B1,[2 1]);
-%   image(B)
+%    image(B)
 [width height dim]=size(B);
 rect=zeros(100,4);
 endloop1=0;
 rectangleCounter=1;
-for rownumber=1:height
+rownumber=1;
+while rownumber<=height
+    inc=1;
     for columnnumber=1:width
         pix=B(columnnumber,rownumber);
         if rect(rectangleCounter,1) > 0
             if pix == 255
                 rect(rectangleCounter,3)=columnnumber-rect(rectangleCounter,1);
                 rectangleCounter=rectangleCounter+1;
-                endloop1=1;
+                
                 break;
             end
         else
@@ -23,6 +25,7 @@ for rownumber=1:height
                     pix2=B(columnnumber,rownumber2);
                     if pix2 == 255
                         rect(rectangleCounter,4)=rownumber2-rect(rectangleCounter,2)-1;
+                        inc=rownumber2-rownumber;
                         break;
                     end
                 end
@@ -32,4 +35,6 @@ for rownumber=1:height
     if endloop1
         break;
     end
+    rownumber=rownumber+inc;
 end
+xlswrite('tuhermana.csv',rect(1:rectangleCounter-1,:));
