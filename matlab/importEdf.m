374d8f11b18ab644b1a70c820d98e254d872d918
Alvaro Neira <alvaroneirareyes@gmail.com>
2015-10-15 19:42:14 -0300

    Added coordinates csvs

---
+++ matlab/importEdf.m
@@ -0,0 +1,13 @@
+%[data,header]=readedf('/Users/aneira/lalo/data/suj2/lalo2-lalo2-23.06.15.15.16.34.edf')
+% EEG.chanlocs(1).labels
+% EEG.chanlocs(2).labels
+% EEG.chanlocs(3).labels
+
+function [ ts, o1, o2 ] = importEdf( filename )
+    EEG = pop_biosig(filename);
+    ts=transpose(EEG.times);
+    eeg=transpose(EEG.data);
+    o1=eeg(:,9);
+    o2=eeg(:,10);
+end
+
