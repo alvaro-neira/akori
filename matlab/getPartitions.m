0e8f6d914f5faaf8c9335d6f76d2f910eb838fce
Alvaro Neira <alvaroneirareyes@gmail.com>
2015-10-23 21:54:58 -0300

    About to truncate to only 1 second

--- matlab/getPartitions.m
+++ matlab/getPartitions.m
@@ -25,14 +25,14 @@ function [P, counters, numberOfFocuses] = getPartitions( websites, focusThreshol
 
     len1=length(allSubjects);
     len2=length(websites);
- 
-   
+    
     numberOfFocuses=getNumberOfFocuses( websites, focusThreshold );
       
     counters=zeros(10,1);
     maxPartitionSize=ceil(numberOfFocuses/10);
 
-    P=zeros(maxPartitionSize,10,4);
+    nFeatures=11;
+    P=zeros(maxPartitionSize,10,nFeatures+1);
     
     rng(rng_settings);
     for persona=1:len1
@@ -52,9 +52,11 @@ function [P, counters, numberOfFocuses] = getPartitions( websites, focusThreshol
             for fijacion=1:len3
                 [ timestamp,pupil_area,gaze_x,gaze_y,saccade,blink ] = getETDataByTimeAndPerson( userFocuses(fijacion,1),...
                      userFocuses(fijacion,2), timestamp_double,pupil_area_filtered,gx,gy,sacc,blk);
-%                 [ timestamp_eeg,eeg_o1,eeg_o2 ] = getEEGDataByTimeAndPerson( userFocuses(fijacion,1),...
-%                         userFocuses(fijacion,2), ts_eeg, o1, o2);
-                
+                 [ timestamp_eeg,eeg_o1,eeg_o2 ] = getEEGDataByTimeAndPerson( userFocuses(fijacion,1),...
+                         userFocuses(fijacion,2), ts_eeg, o1, o2);
+                [ A4_o1, D4_o1, D3_o1, D2_o1, A1_o1 ] = getWaveletCoefficients( eeg_o1 );
+                [ A4_o2, D4_o2, D3_o2, D2_o2, A1_o2 ] = getWaveletCoefficients( eeg_o2 );
+
                 rand10 = randi(10);
                 plusOne=counters(rand10)+1;
                 if counters(1)<maxPartitionSize-1 || ...
@@ -72,17 +74,13 @@ function [P, counters, numberOfFocuses] = getPartitions( websites, focusThreshol
                     while plusOne > maxPartitionSize-1
                         rand10 = randi(10);
                         plusOne=counters(rand10)+1;
-%                         if(plusOne>180)
-%                             rand10
-%                             plusOne
-%                             counters
-%                         end
+
                     end
                 else
                     while plusOne > maxPartitionSize
                         rand10 = randi(10);
                         plusOne=counters(rand10)+1;
-%                         
+                        
                     end
                    
                 end   
@@ -93,7 +91,16 @@ function [P, counters, numberOfFocuses] = getPartitions( websites, focusThreshol
                 P(counters(rand10),rand10,2)=ddiff;
                 [cdiff,cindex]=contraccionMaxima(pupil_area);
                 P(counters(rand10),rand10,3)=cdiff;
-                P(counters(rand10),rand10,4)=isHombre(subjectId);
+                P(counters(rand10),rand10,4)=std(D2_o1);  
+                P(counters(rand10),rand10,5)=std(A1_o1); 
+                P(counters(rand10),rand10,6)=sumsqr(D2_o1); 
+                P(counters(rand10),rand10,7)=sumsqr(A1_o1); 
+                P(counters(rand10),rand10,8)=std(D2_o2); 
+                P(counters(rand10),rand10,9)=std(A1_o2); 
+                P(counters(rand10),rand10,10)=sumsqr(D2_o2); 
+                P(counters(rand10),rand10,11)=sumsqr(A1_o2); 
+                
+                P(counters(rand10),rand10,nFeatures+1)=isHombre(subjectId);
             end
         end
         
--- matlab/instrucciones_2_5_2.m
+++ matlab/instrucciones_2_5_2.m
@@ -21,7 +21,8 @@ global visionfilelist;
 global subjectWithoutET;
 global subjectWithoutEEG;
 global rng_settings;
-
+global HOMBRE;
+global MUJER;
 hardCodedData();
 
 %Male Websites
@@ -31,11 +32,12 @@ nFeatures=depth-1;
 for partitionToSkip=1:nPartitions
     heightToSkip=counters(partitionToSkip);
     height=totalSamples-heightToSkip;
-
-   
+    Ytest=zeros(heightToSkip,1);
+    Ypredicted=zeros(heightToSkip,1);
     
     Y=[cellstr('none');cellstr('none')];
     X=zeros(height,nFeatures);
+    Xtest=zeros(heightToSkip,nFeatures);
     ycounter=0;
     for partitionNumber=1:nPartitions
         if partitionNumber==partitionToSkip
@@ -43,9 +45,9 @@ for partitionToSkip=1:nPartitions
         end
         for i=1:counters(partitionNumber)
             if P(i,partitionNumber,depth)
-                Y(ycounter+i)=cellstr('hombre');
+                Y(ycounter+i)=cellstr(HOMBRE);
             else
-                Y(ycounter+i)=cellstr('mujer');
+                Y(ycounter+i)=cellstr(MUJER);
             end
             for j=1:nFeatures
                 X(ycounter+i,j)=P(i,partitionNumber,j);
@@ -53,7 +55,33 @@ for partitionToSkip=1:nPartitions
         end
         ycounter=ycounter+counters(partitionNumber);
     end
-    SVMModel=fitcsvm(X,Y,'KernelFunction','rbf','Standardize',true,'ClassNames',{'mujer','hombre' });
+    SVMModel=fitcsvm(X,Y,'KernelFunction','rbf','Standardize',true,'ClassNames',{MUJER,HOMBRE });
+    
+    %Testing
+    testLength=heightToSkip;
+    for i=1:testLength
+        for j=1:nFeatures
+            Xtest(i,j)=P(i,partitionToSkip,j);
+        end
+    end
+    for i=1:testLength
+        Ytest(i)=P(i,partitionToSkip,depth);
+    end
+    labels = predict(SVMModel,Xtest); 
+    for i=1:testLength
+        Ypredicted(i)=strcmp(labels(i),HOMBRE);
+    end
+    
+    C=confusionmat(Ytest,Ypredicted);
+    VP=C(1,1);
+    FN=C(1,2);
+    FP=C(2,1);
+    VN=C(2,2);
+    recall= VP/(VP+FN);
+    precision= VP/(VP+FP);
+    accuracy= (VP+VN)/(VP+VN+FP+FN);
+
+    disp(strcat('recall=',num2str(recall),',precision=',num2str(precision),',accuracy=',num2str(accuracy)));
 
 end
 
@@ -61,6 +89,7 @@ end
 % std_pupil_area male
 % dMax male
 % cMax male
+            
 % std_D2_o1 male
 % std_A1_o1 male
 % energy_D2_o1 male
